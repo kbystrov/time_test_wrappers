dc5eee39aff4931c193b58b49efe9b4f069c880e
kbystrov <kirill.bystrov@phystech.edu>
2020-07-02 13:55:12 +0300

    Minor updates

--- vdso/test_vdso.c
+++ vdso/test_vdso.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <stdint.h>
 
-int
-main(int argc, char *argv[])
-{
+static const uint64_t nsecs_in_sec = 1e9;
+static const uint64_t iters = 1e6;
+
+int main(int argc, char *argv[]){
 	/*
         struct timeval tv;
         int i = 0;
@@ -13,11 +15,36 @@ main(int argc, char *argv[])
                 gettimeofday(&tv,NULL);
         }
         */
-        struct timespec ts;									
+
+        uint64_t real_sec = 0;
+        uint64_t real_nsec = 0;
+        double real_time = 0;
+
+        struct timespec ts, tv_st, tv_end;
+
+        clock_gettime(CLOCK_MONOTONIC, &tv_st);
+
         int j = 0;
-        for (; j<1000000; j++) {
+        for (; j<iters; j++) {
                 clock_gettime(CLOCK_MONOTONIC, &ts);
         }
+
+        clock_gettime(CLOCK_MONOTONIC, &tv_end);
+
+        real_sec = tv_end.tv_sec - tv_st.tv_sec;
+
+        if(tv_end.tv_nsec < tv_st.tv_nsec){
+            real_sec -= 1;
+            real_nsec = tv_end.tv_nsec + (nsecs_in_sec - tv_st.tv_nsec);
+        } else {
+            real_nsec = tv_end.tv_nsec - tv_st.tv_nsec;
+        }
+
+        real_time = real_sec +  (double) real_nsec / nsecs_in_sec ;
+
+        printf("Real time: %2.9f\n" , real_time );
+
+        printf("Avg time latency of %ld calls to clock_gettime = %.0f nsecs\n", iters, real_time * nsecs_in_sec / iters  );
 
 
         return 0;
